348cb12771eb8426946deb0e698f051c968f072a
Daniil Gentili <daniil@daniil.it>
2022-05-06 13:32:55 +0200

    Revert debug

--- src/Target/Php80/StaticMixedFalseReplacer.php
+++ src/Target/Php80/StaticMixedFalseReplacer.php
@@ -2,11 +2,8 @@
 
 namespace Phabel\Target\Php80;
 
-use Phabel\Context;
 use Phabel\Plugin;
 use Phabel\Plugin\TypeHintReplacer;
-use PhpParser\Node\Expr\Assign;
-use PhpParser\Node\Expr\AssignOp;
 
 /**
  * Replace static, mixed and false typehints.
@@ -16,20 +13,6 @@ use PhpParser\Node\Expr\AssignOp;
  */
 class StaticMixedFalseReplacer extends Plugin
 {
-    public function enter(Assign $node, Context $context)
-    {
-        if (!$context->isParentStmt()) {
-            $context->insertBefore($node, clone $node);
-            return $node->var;
-        }
-    }
-    public function enterOp(AssignOp $node, Context $context)
-    {
-        if (!$context->isParentStmt()) {
-            $context->insertBefore($node, clone $node);
-            return $node->var;
-        }
-    }
     public static function previous(array $config): array
     {
         return [
